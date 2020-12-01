a8362240d3297d9d8a024ce72c8a0312226e9e52
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-12-01 20:44:45 +0100

    Add vue router Add basic structure of header, about and contact components

---
+++ src/Components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <header class='header'>
+      <h3>Hello</h3>
+      <nav>
+        <span><router-link to='/' tag='span' exact >About</router-link></span>
+        <span><router-link to='/about' tag='span' exact >Contact</router-link></span>
+      </nav>
+    </header>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+header {
+  background-color: brown;
+  color: white;
+  height: 80px;
+  line-height: 80px;
+  padding: 0 20px;
+  font-size: 20px;
+}
+
+h3 {
+  display: inline-block;
+  margin: 0;
+}
+
+nav {
+  float: right;
+}
+
+span {
+  padding: 5px 20px;
+}
+</style>
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import AboutComp from './Components/About.vue';
+import ContactComp from './Components/Contact.vue';
+
+export const routes = [
+  { path: '', component: AboutComp },
+  { path: '/contact', component: ContactComp }
+]
+
